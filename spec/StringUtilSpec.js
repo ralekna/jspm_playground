9a91ac703b77a1be8615fda1d9ef519dae35fd37
Rytis Alekna <r.alekna@gmail.com>
2015-04-21 20:05:10 +0300

    added karma test runnner that does not transpile normaly

---
+++ spec/StringUtilSpec.js
@@ -0,0 +1,10 @@
+/**
+ * Created by rytis on 4/21/15.
+ */
+import StringUtils from 'lib/StringUtils';
+
+describe('String utils specs', function () {
+  it('just have to run', function () {
+    expect(true).toBe(true);
+  });
+});
