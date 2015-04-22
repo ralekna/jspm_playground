e938eb230159e50407161b6fe6e41644552dd819
Rytis Alekna <rytis@samesystem.com>
2015-04-22 17:57:42 +0300

    Added es6 templates spec that breaks karma runner

---
+++ spec/Es6TemplatesSpec.js
@@ -0,0 +1,18 @@
+/**
+ * Created by rytis on 2015-04-22.
+ */
+
+describe('String utils specs 2', () => {
+  it('just have to run 2', () => {
+    expect(true).toBe(true);
+  });
+});
+
+describe('Various es6 templates functionality', () => {
+  it('should interpolate local vars', () => {
+    debugger;
+    let one = '1';
+    let two = '2';
+    // expect(`${one}${two}`).toBe('12');
+  });
+});
